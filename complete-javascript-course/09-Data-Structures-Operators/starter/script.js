84bdbfdc4e9820f516e3e5a6632470a575dfc5a8
Jade Woods <jadewoods@Jades-MacBook-Air.local>
2022-01-11 15:55:42 -0800

    revisiting javascript

--- complete-javascript-course/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course/09-Data-Structures-Operators/starter/script.js
@@ -5,12 +5,13 @@
 //   '_Delayed_Departure;fao93766109;txl2133758440;11:25+_Arrival;bru0943384722;fao93766109;11:45+_Delayed_Arrival;hel7439299980;fao93766109;12:05+_Departure;fao93766109;lis2323639855;12:30';
 
 // Data needed for first part of the section
+const weekdays = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
 const hours = {
-  thu: {
+  [weekdays[3]]: {
     open: 12,
     close: 22,
   },
-  fri: {
+  [weekdays[4]]: {
     open: 11,
     close: 23,
   },
@@ -27,11 +28,17 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  //ES6 enhanced object literals
   hours,
 
-  order(starterIndex, mainIndex) {
+  order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
+
+  //^^ THIS CAN BE SHORTEND TO ^^//
+  // order(starterIndex, mainIndex) {
+  //   return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
+  // },
 
   orderDelivery: function ({
     starterIndex = 1,
@@ -48,6 +55,10 @@ const restaurant = {
     console.log(
       `Here is your delicious pasta with ${ing1}, ${ing2}, and ${ing3}.`
     );
+
+    //To Call The Above Function:
+    //restaurant.orderPasta(...ingredients);
+    //This will get the array prompt answers into the orderPasta console log
   },
 
   orderPizza: function (mainIngredient, ...otherIngredients) {
@@ -56,13 +67,107 @@ const restaurant = {
   },
 };
 
-const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+//property NAMES
+// const properties = Object.keys(hours);
+// console.log(properties);
 
-for (const item of menu) console.log(item);
+// let openStr = `We are open on ${properties.length} days: `;
 
-for (const [i, el] of menu.entries()) {
-  console.log(`${i + 1}: ${el}`);
-}
+// for (const day of properties) {
+//   openStr += `${day}, `;
+// }
+// console.log(openStr);
+
+// //PROPERT VALUES
+// const values = Object.values(hours);
+// console.log(values);
+
+// //Entrie Object
+// const entries = Object.entries(hours);
+// console.log(entries);
+
+// for (const [day, { open, close }] of entries) {
+//   console.log(`On ${day} we open at ${open} and close at ${close}`);
+// }
+
+// if (restaurant.hours.fri.open) console.log(restaurant.hours.fri.open);
+
+// //WITH optional chaining
+// console.log(restaurant.hours.mon?.open);
+
+// //Example
+// const days = ['mon', 'tue', 'wed', 'thur', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.hours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// // THE ?? IS FOR NULL AND UNDEFINED//
+
+// //Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// //Arrays
+// const users = [{ name: 'Jade', email: 'hello@jade.io' }];
+// console.log(users[0]?.name ?? 'user array empty');
+//^^If users[0] element does exist, then give us its name^^//
+
+// const properties = Object.keys(hours);
+// console.log(properties);
+
+// let openStr = `We are open on ${properties.length} days: `;
+
+// for (const day of properties) {
+//   openStr += `${day},`;
+// }
+
+// console.log(openStr);
+
+//Property Values
+
+// const values = Object.values(hours);
+// console.log(values);
+
+//Entrie Object
+// const entries = Object.entries(hours);
+// console.log(entries);
+
+// for (const [key, { open, close }] of entries) {
+//   console.log(`On ${key} we open at ${open} and close at ${close}`);
+// }
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+// for (const day of days) {
+//   const open = restaurant.hours[day]?.open ?? 'closed';
+//   console.log(`On ${day}, we open at ${open}`);
+// }
+
+// //Methods
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not exist');
+
+// //Arrays
+// const users = [{ name: 'Jade', email: 'hello@jade.io' }];
+
+// console.log(users[0]?.name ?? 'User array empty');
+
+//Optional Chaining
+
+//The for-of Loop
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const item of menu.entries()) {
+//   // console.log(item);
+//   console.log(`${item[0] + 1}: ${item[1]}`);
+// }
+
+// console.log([...menu.entries()]);
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
 
 // const rest1 = {
 //   name: 'Capri',
@@ -74,19 +179,24 @@ for (const [i, el] of menu.entries()) {
 //   owner: 'Giovanni Rossi',
 // };
 
-// //ASSIGNMENT OPERATOR
+// //OR ASSIGNMENT OPERATOR
 // // rest1.numGuests = rest1.numGuests || 10;
 // // rest2.numGuests = rest2.numGuests || 10;
+
+//Shorter Way
 // // rest1.numGuests ||= 10;
 // // rest2.numGuests ||= 10;
 
-// //NULLISH ASSIGNMENT OPERATOR
+// //NULLISH ASSIGNMENT OPERATOR (Null or Undefined)
 // rest1.numGuests ??= 10;
 // rest2.numGuests ??= 10;
+
+//AND ASSIGNMENT OPERATOR
 
 // rest2.owner = rest2.owner && '<ANONYMOUS>';
 // rest1.owner = rest1.owner && '<ANONYMOUS>';
 
+//Shorter Way
 // rest1.owner &&= '<ANONYMOUS>';
 // rest2.owner &&= '<ANONYMOUS>';
 
@@ -113,7 +223,7 @@ for (const [i, el] of menu.entries()) {
 // const { sat, ...weekDays } = restaurant.openingHours;
 // console.log(weekDays);
 
-// //Functions
+// //Spreading Arrays in Functions
 // const add = function (...numbers) {
 //   let sum = 0;
 //   for (let i = 0; i < numbers.length; i++) sum += numbers[i];
@@ -232,3 +342,264 @@ for (const [i, el] of menu.entries()) {
 // //Default Values
 // const [p = 1, q = 1, r = 1] = [8, 9];
 // console.log(p, q, r);
+
+//CODING CHALLENGE 1//
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrusia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alba',
+//       'Davies',
+//       'Kimich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lebowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lebowski', 'Gnarby', 'Lebowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// const players1 = [
+//   'Neuer',
+//   'Pavard',
+//   'Martinez',
+//   'Alba',
+//   'Davies',
+//   'Kimich',
+//   'Goretzka',
+//   'Coman',
+//   'Muller',
+//   'Gnarby',
+//   'Lebowski',
+// ];
+// const players2 = [
+//   'Burki',
+//   'Schulz',
+//   'Hummels',
+//   'Akanji',
+//   'Hakimi',
+//   'Weigl',
+//   'Witsel',
+//   'Hazard',
+//   'Brandt',
+//   'Sancho',
+//   'Gotze',
+// ];
+
+// const gk = 'Neuer';
+// const fieldPlayers = [
+//   'Pavard',
+//   'Martinez',
+//   'Alba',
+//   'Davies',
+//   'Kimich',
+//   'Goretzka',
+//   'Coman',
+//   'Muller',
+//   'Gnarby',
+//   'Lebowski',
+// ];
+
+// const allPlayers = [...players1, ...players2];
+// const players1Final = ['Thiago', 'Coutinho', 'Perisic', ...players1];
+
+// const {
+//   odds: { team1, x: draw, team2 },
+// } = game;
+
+// const printGoals = function (...players) {
+//   console.log(players);
+//   console.log(`${players.length} goals were scored`);
+// };
+
+// printGoals(...game.scored);
+
+// team1 < team2 && console.log('Team 1 is more likely to win');
+// team1 > team2 && console.log('Team 2 is more likely to win');
+
+// console.log(true && 'nullx');
+
+// const team1 = game.odds.team1;
+
+// const { odds } = game;
+// const { team1 } = odds
+
+// const { odds } = game;
+
+// const hello = game.odds;
+
+// odds: {
+//   team1: 1.33,
+//   x: 3.25,
+//   team2: 6.5,
+
+// const game = {
+//   team1: 'Bayern Munich',
+//   team2: 'Borrusia Dortmund',
+//   players: [
+//     [
+//       'Neuer',
+//       'Pavard',
+//       'Martinez',
+//       'Alba',
+//       'Davies',
+//       'Kimich',
+//       'Goretzka',
+//       'Coman',
+//       'Muller',
+//       'Gnarby',
+//       'Lebowski',
+//     ],
+//     [
+//       'Burki',
+//       'Schulz',
+//       'Hummels',
+//       'Akanji',
+//       'Hakimi',
+//       'Weigl',
+//       'Witsel',
+//       'Hazard',
+//       'Brandt',
+//       'Sancho',
+//       'Gotze',
+//     ],
+//   ],
+//   score: '4:0',
+//   scored: ['Lebowski', 'Gnarby', 'Lebowski', 'Hummels'],
+//   date: 'Nov 9th, 2037',
+//   odds: {
+//     team1: 1.33,
+//     x: 3.25,
+//     team2: 6.5,
+//   },
+// };
+
+// let pScored = game.scored;
+// const gameFinal1 = game.team1;
+// const gameFinal2 = game.team2;
+
+// for (const [index, value] of pScored.entries()) {
+//   console.log(`Goal ${index + 1}: ${value}`);
+// }
+
+// const betOdds = game.odds;
+
+// let avg = 0;
+// for (const key in betOdds) {
+//   avg = avg + betOdds[key];
+// }
+// avg = avg / Object.keys(betOdds).length;
+// console.log(Math.round(avg));
+
+// for (const el in betOdds) {
+//   let word = '';
+
+//   if (game[el]) {
+//     word = `victory ${game[el]}`;
+//   } else {
+//     word = 'draw';
+//   }
+
+//   console.log(`Odd of ${word}: ${betOdds[el]}`);
+
+//   // console.log(el);
+//   // if()
+// }
+// console.log(betOdds);
+
+const game = {
+  team1: 'Bayern Munich',
+  team2: 'Borrusia Dortmund',
+  players: [
+    [
+      'Neuer',
+      'Pavard',
+      'Martinez',
+      'Alba',
+      'Davies',
+      'Kimich',
+      'Goretzka',
+      'Coman',
+      'Muller',
+      'Gnarby',
+      'Lebowski',
+    ],
+    [
+      'Burki',
+      'Schulz',
+      'Hummels',
+      'Akanji',
+      'Hakimi',
+      'Weigl',
+      'Witsel',
+      'Hazard',
+      'Brandt',
+      'Sancho',
+      'Gotze',
+    ],
+  ],
+  score: '4:0',
+  scored: ['Lebowski', 'Gnarby', 'Lebowski', 'Hummels'],
+  date: 'Nov 9th, 2037',
+  odds: {
+    team1: 1.33,
+    x: 3.25,
+    team2: 6.5,
+  },
+};
+
+const playersSored = game.scored;
+
+for (const [index, player] of playersSored.entries()) {
+  console.log(`Goal ${index + 1}: ${player}`);
+}
+
+const betOdds = game.odds;
+
+let avg = 0;
+for (const key in betOdds) {
+  avg = avg + betOdds[key];
+}
+avg = avg / Object.keys(betOdds).length;
+console.log(Math.round(avg));
+
+for (const el in betOdds) {
+  let word = '';
+
+  if (game[el]) {
+    word = `victory ${game[el]}`;
+  } else {
+    word = 'draw';
+  }
+  console.log(`Odd of ${word}: ${betOdds[el]}`);
+
+  // console.log(el);
+}
+console.log(betOdds);
