66ad84330ea678d590d7744da86117320bbc5c4c
Jade Woods <jadewoods@Jades-MacBook-Air.local>
2022-01-18 19:33:20 -0800

    app building practice

--- complete-javascript-course/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course/11-Arrays-Bankist/starter/script.js
@@ -77,22 +77,36 @@ const displayMovements = function (movements) {
   });
 };
 
-displayMovements(account1.movements);
+// displayMovements(account1.movements);
 
 const calcDisplayBalance = function (movements) {
   const balance = movements.reduce((acc, mov) => acc + mov, 0);
   labelBalance.textContent = `${balance} £`;
 };
-calcDisplayBalance(account1.movements);
+// calcDisplayBalance(account1.movements);
 
-const calcDisplaySummary = function (movements) {
-  const incomes = movements
+const calcDisplaySummary = function (acc) {
+  const incomes = acc.movements
     .filter(mov => mov > 0)
     .reduce((acc, mov) => acc + mov, 0);
   labelSumIn.textContent = `${incomes}€`;
+
+  const out = acc.movements
+    .filter(mov => mov < 0)
+    .reduce((acc, mov) => acc + mov, 0);
+  labelSumOut.textContent = `${Math.abs(out)}€`;
+
+  const interest = acc.movements
+    .filter(mov => mov > 0)
+    .map(deposit => (deposit * acc.interestRate) / 100)
+    .filter((int, i, arr) => {
+      return int >= 1;
+    })
+    .reduce((acc, int) => acc + int, 0);
+  labelSumInterest.textContent = `${Math.abs(interest)}€`;
 };
 
-calcDisplaySummary(account1.movements);
+// calcDisplaySummary(account1.movements);
 
 const createUsernames = function (accs) {
   accs.forEach(function (acc) {
@@ -107,6 +121,40 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts); // stw
+
+//EVENT HANDLER//
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  //Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome Back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    //Clear Input Fields//
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    //Display Movements
+    displayMovements(currentAccount.movements);
+
+    //Display Balance
+    calcDisplayBalance(currentAccount.movements);
+    //Display Summary
+    calcDisplaySummary(currentAccount);
+  }
+});
 
 // console.log(accounts);
 
@@ -348,11 +396,29 @@ createUsernames(accounts); // stw
 // console.log(calcAverageHumanAge(ages2));
 
 //Chaining Methods//
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+
+// const totalDepositISD = movements
+//   .filter(mov => mov > 0)
+//   .map(mov => mov * 1.1)
+//   .reduce((acc, mov) => acc + mov, 0);
+
+// console.log(totalDepositISD);
+
+// //Coding Challenge #3//
+// const array1 = [5, 2, 4, 1, 15, 8, 3];
+// const array2 = [16, 6, 10, 5, 6, 1, 4];
+
+// const calcAverageHumanAge = array1
+//   .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+//   .filter(age => age >= 18)
+//   .reduce((acc, age, i, arr) => acc + age / arr.length, 0);
+
+// console.log(calcAverageHumanAge);
+
+//Find Method//
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
-const totalDepositISD = movements
-  .filter(mov => mov > 0)
-  .map(mov => mov * 1.1)
-  .reduce((acc, mov) => acc + mov, 0);
+const firstWithdrawal = movements.find(mov => mov < 0);
 
-console.log(totalDepositISD);
+const account = accounts.find(acc => acc.owner === 'Jessica Davis');
